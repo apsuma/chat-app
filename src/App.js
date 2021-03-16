6fed82c24c1a50ae0ed07a041f89c0408925691c
apsuma <delphine.belet@gmail.com>
2021-03-16 22:19:22 +0100

    add ContactList to project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-import Contact from './components/Contact'
-const contact1 = {
-  avatar: 'https://randomuser.me/api/portraits/women/65.jpg',
-  name: 'Erica Fowler',
-  online: true
-}
-const contact2 = {
-  avatar: 'https://randomuser.me/api/portraits/men/37.jpg',
-  name: 'Herbert Bryant',
-  online: false
-}
-const contact3 = {
-  avatar: 'https://randomuser.me/api/portraits/men/52.jpg',
-  name: 'Ryan Sanchez',
-  online: true
-}
+import ContactList from './components/ContactList'
+
 
 function App() {
   return (
     <div className="App">
-      <Contact 
-        name={contact1.name}
-        avatar={contact1.avatar}
-        status={contact1.online} 
-      />
-      <Contact 
-        name={contact2.name}
-        avatar={contact2.avatar}
-        status={contact2.online} 
-      />
-      <Contact 
-        name={contact3.name}
-        avatar={contact3.avatar}
-        status={contact3.online} 
-      />
+      <ContactList />
     </div>
   );
 }
